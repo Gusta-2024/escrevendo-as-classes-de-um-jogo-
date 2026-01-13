f9b2c4cb53c577236180d4ddb8fb101026062e4c
Gustavo_25 <gustavo.h.n.godoi@gmail.com>
2026-01-13 16:08:30 +0000

    inserção de imagem

--- classe.js
+++ classe.js
@@ -1,8 +1,9 @@
 class Heroi{
-    constructor(nome, idade, tipo){
+    constructor(nome, idade, tipo, imagemUrl){
         this.nome = nome;
         this.level = idade;
         this.tipo = tipo;
+        this.imagem = imagemUrl;
     }
 
     atacar() {
@@ -17,14 +18,17 @@ class Heroi{
         // Agora ele atualiza o HTML em vez de só dar log
         const display = document.getElementById("mensagem-ataque");
         display.innerText = `O herói ${this.nome} (${this.tipo}) atacou usando ${ataque}!`;
+
+        display.style.opacity = 0;
+        setTimeout(() => display.style.opacity = 1, 100);
     }
 }
          
 const herois = [
-    new Heroi("gandalf", "80", "mago"),
-    new Heroi("baldur", "56", "guerreiro"),
-    new Heroi("oden", "70", "monge"),
-    new Heroi("snakeice", "38", "ninja")
+    new Heroi("Gandalf", "80", "mago", "https://i.pinimg.com/736x/8f/c2/f7/8fc2f727d925d57b44f2d59d2e1b1d7d.jpg"),
+    new Heroi("Baldur", "56", "guerreiro", "https://i.pinimg.com/564x/0f/60/da/0f60da867c2657d42817351dfdf2929e.jpg"),
+    new Heroi("Oden", "70", "monge", "https://i.pinimg.com/564x/9c/0b/22/9c0b22f033c4df29da68487707e7774d.jpg"),
+    new Heroi("Snakeice", "38", "ninja", "https://i.pinimg.com/564x/e7/7e/16/e77e169c2889988137356c5267023363.jpg")
 ];
 
 const container = document.getElementById("hero-selection");
